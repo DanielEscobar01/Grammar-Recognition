0683bcb638200fd73dda942ffe2d42b45af351b9
danielescobar <danielescobar@localhost>
2020-04-09 08:12:43 -0500

    Grammar Recognition

---
+++ src/grammars/Grammar.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grammars;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class let us create a grammar
+ *
+ * @author danielescobar
+ */
+public class Grammar {
+
+    private List<Production> productions = new ArrayList<>();
+
+    /**
+     * Constructor to create a grammar without any productions
+     */
+    public Grammar() {
+
+    }
+
+    /**
+     * Constructor to create a grammar with an initial production
+     *
+     * @param initialProduction Is the initial production of the grammar
+     */
+    public Grammar(Production initialProduction) {
+        this.productions.add(initialProduction);
+    }
+
+    /**
+     * This method let us add a new production to the grammar
+     *
+     * @param production
+     */
+    public void addProduction(Production production) {
+        this.productions.add(production);
+    }
+
+    /**
+     * This Method let us reinitialize our grammar, to create a new one.
+     */
+    public void reinitialize() {
+        productions.clear();
+    }
+
+    /**
+     * This method let us know if the grammar is well defined
+     *
+     * @return
+     */
+    public boolean isCorrect() {
+        List<String> namesLeft = new ArrayList<>();
+        List<String> namesRight = new ArrayList<>();
+        for (int i = 0; i < getProductions().size(); i++) {
+            namesLeft.add(getProductions().get(i).getLeftSide().getID());
+        }
+        for (int i = 0; i < getProductions().size(); i++) {
+            for (int j = 0; j < getProductions().get(i).nonTerminalsInRight().size(); j++) {
+                namesRight.add(getProductions().get(i).nonTerminalsInRight().get(j).getID());
+            }
+        }
+        if (namesLeft.size() < namesRight.size()) {
+            return false;
+        } else {
+            for (int i = 0; i < namesRight.size(); i++) {
+                int flag = 0; // 0 means it didnt find the terminal in left
+                for (int j = 0; j < namesLeft.size(); j++) {
+                    if (namesRight.get(i).equals(namesLeft.get(j))) {
+                        flag = 1;
+                    }
+                }
+                if(flag==0){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public List<Production> getProductions() {
+        return this.productions;
+    }
+}
---
+++ src/grammars/Production.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grammars;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class let us create a production
+ *
+ * @author danielescobar
+ */
+public class Production {
+
+    private NonTerminal leftSide;
+    List<Object> rightSide = new ArrayList<>();
+    List<NonTerminal> nonTerminalsInRight = new ArrayList<>();
+
+    /**
+     * Constructor to create a empty production.
+     */
+    public Production() {
+
+    }
+
+    /**
+     * Constructor to create a production for the grammar
+     *
+     * @param leftSide Is the left side of the production
+     */
+    public Production(NonTerminal leftSide) {
+        setLeftSide(leftSide);
+    }
+
+    /**
+     * This method let us set the left side value of the production
+     *
+     * @param leftSide Is the left side of the production
+     */
+    public void setLeftSide(NonTerminal leftSide) {
+        this.leftSide = leftSide;
+    }
+
+    /**
+     * This method let us know the non terminal of the left side
+     *
+     * @return The non terminal of the left side
+     */
+    public NonTerminal getLeftSide() {
+        return this.leftSide;
+    }
+
+    /**
+     * This method let us add a non terminal to the right side of the production
+     *
+     * @param rightSide Element to be added to the right side of the production
+     */
+    public void addElementRightSideN(NonTerminal rightSide) {
+        this.rightSide.add(rightSide);
+        this.nonTerminalsInRight.add(rightSide);
+    }
+
+    /**
+     * This method let us add a terminal to the right side of the production
+     *
+     * @param rightSide Element to be added to the right side of the production
+     */
+    public void addElementRightSide(Terminal rightSide) {
+        this.rightSide.add(rightSide);
+    }
+
+    /**
+     * This method let us know the first item of the right side
+     *
+     * @return the terminal or non terminal in the first part of the right side
+     */
+    public Object firstItem() {
+        if (rightSide.isEmpty()) {
+            return null;
+        }
+        return rightSide.get(0);
+    }
+
+    /**
+     * This method let us know if the first item is a terminal
+     *
+     * @return True if the first item is terminal, otherwise return False
+     */
+    public boolean firstItemIsTerminal() {
+        return firstItem().getClass().toString().equals("class grammars.Terminal");
+    }
+
+    /**
+     * This method let us know if the first item of the right side is lambda
+     *
+     * @return True if the first element is lambda, otherwise returns False
+     */
+    public boolean firstItemIsLambda() {
+        return firstItem() == null;
+    }
+
+    /**
+     * This method let us know the terminals defined in the right side
+     *
+     * @return
+     */
+    public List<NonTerminal> nonTerminalsInRight() {
+        return this.nonTerminalsInRight;
+    }
+
+}
