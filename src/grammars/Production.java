c3169c609e6c7cf1f9643f48e3876a4a2c60e65e
danielescobar <danielescobar@localhost>
2020-05-26 23:38:44 -0500

    App

--- src/grammars/Production.java
+++ src/grammars/Production.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class Production {
 
     
+    
     private NonTerminal leftSide;
     List<Object> rightSide = new ArrayList<>();
     List<NonTerminal> nonTerminalsInRight = new ArrayList<>();
